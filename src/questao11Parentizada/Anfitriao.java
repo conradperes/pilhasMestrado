16b67f2446583032adaaf23f8e3daa11bffe5878
conradperes <conrad.marques.peres@gmail.com>
2018-05-08 21:54:20 -0300

    Estimativa de complexidade

--- src/questao11Parentizada/Anfitriao.java
+++ src/questao11Parentizada/Anfitriao.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
  * @author Conrad Peres
  */
 public class Anfitriao {
+	/*
+	 * Complexidade O(N)
+	 */
     public static void main(String[] args) {
         System.out.println("Digite a expressao: ");
         Scanner sc = new Scanner(System.in);
